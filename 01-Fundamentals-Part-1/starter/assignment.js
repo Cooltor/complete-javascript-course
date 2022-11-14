7b76ca29719d1c30e6ebc30b244ee9667f110a71
Cooltor <romaindubus86@gmail.com>
2022-11-14 19:41:22 +0100

    Fundamentals 1

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,8 @@
+/*
+let country = "France";
+let continent = "Europe";
+let population = 64000000;
+
+console.log(country, continent, population);
+*/
+
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,60 @@
+/* console.log("Hello world");
+
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+
+let PI = 3.1415;
+console.log(PI);
+
+let myFirstJob = 'Programmer';
+let currentJob = 'Teacher';
+
+console.log(myFirstJob);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'jonas');
+
+javascriptIsFun = 'YES';
+console.log(typeof javascriptIsFun);
+
+console.log(typeof null); 
+*/
+
+/*
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 2, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3=2*2*2
+
+const firstName = 'Jonas';
+const lastName = 'Brother';
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x*4 = 100
+x++; // x =x + 1
+x--;
+console.log(x);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(now - 1991 > now - 2018);
+
+console.log("Malik c'est le plus costaud");
+
