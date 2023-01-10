5a81db614d00ddb9a2c325199a7edc578dbe17c4
Cooltor <romaindbus86@gmail.com>
2023-01-10 12:34:44 +0100

    SET MAPS done

--- 09-Data-Structures-Operators/starter/app.js
+++ 09-Data-Structures-Operators/starter/app.js
@@ -41,9 +41,81 @@ const game = {
   },
 };
 
-// Coding Challenge #2
+// Coding Challenge #3
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+//1
+const gameEventsSet = [...new Set(gameEvents.values())];
+console.log(gameEventsSet);
+
+//2
+gameEvents.delete(64);
+console.log(gameEvents);
+
+//3
+
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes.`
+);
+
+const duration = [...gameEvents.keys()].pop();
+
+console.log(
+  `An event happened, on average, every ${duration / gameEvents.size} minutes.`
+);
+
+//4
+for (const [min, ev] of gameEvents) {
+  let halfTime = min <= 45 ? '[FIRST HALF]' : '[SECOND HALF]';
+  console.log(`${halfTime} ${min}: ${ev}`);
+}
 
 /*
+// Coding Challenge #2
+
+// 1
+
+let gameScored = game?.scored.entries();
+
+for (const [i, el] of gameScored) {
+  console.log(`Goal ${i + 1}: ${el}`);
+}
+
+//2
+
+//calcAverage(odd);
+
+const odds = Object.values(game?.odds);
+console.log(odds);
+let average = 0;
+for (let odd of odds) average += odd;
+
+let result = average / odds.length;
+console.log(result);
+
+//3
+
+for (const [team, odd] of Object.entries(game.odds)) {
+  let teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr}: ${odd}`);
+}
+
+
+
+// Coding Challenge #1
+
 //1
 //const players1 = game.players[0];
 //const players2 = game.players[1];
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -315,4 +315,105 @@ for (const [key, { open, close }] of entries) {
   console.log(`on ${key} we open at ${open} and close at ${close}`);
 }
 
+
+
+const ordersSet = new Set([
+  'Pasta',
+  'Pizza',
+  'Pizza',
+  'Risotto',
+  'Pasta',
+  'Pizza',
+]);
+
+console.log(ordersSet);
+
+console.log(new Set('Jonas'));
+console.log(ordersSet.size);
+console.log(ordersSet.has('Pizza'));
+console.log(ordersSet.has('Bread'));
+ordersSet.add('Garlic Bread');
+ordersSet.add('Garlic Bread');
+ordersSet.delete('Risotto');
+// ordersSet.clear()
+console.log(ordersSet);
+
+for (const order of ordersSet) console.log(order);
+
+// example
+
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+console.log(
+  new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
+);
+
+console.log(new Set('Scheherazade').size);
+
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('Categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open')
+  .set(false, 'We are closed');
+
+console.log(rest.get('name'));
+//console.log(rest.get(true));
+
+const time = 8;
+
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+rest.set([1, 2], 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+// rest.clear()
+
+
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🏆'],
+  [false, 'Try again!'],
+]);
+
+console.log(question);
+
+// Convert Object to Map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Quizz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+
+const answer = Number(prompt('Your answer'));
+console.log(answer);
+
+//answer === question.get('correct')
+//  ? console.log(question.get(true))
+//  : console.log(question.get(false));
+
+console.log(question.get(question.get('correct') === answer));
+
+// Convert map to array
+console.log([...question]);
+console.log(...question.keys());
+console.log(...question.values());
 */
